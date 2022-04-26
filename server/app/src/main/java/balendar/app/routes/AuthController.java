defaf2ec464ed665393ee9747ade87c5fa562b58
JoshuaXOng <joshuaong2000@gmail.com>
2022-04-26 18:54:39 +1000

    fix: fixed bad gson conversion

--- server/app/src/main/java/balendar/app/routes/AuthController.java
+++ server/app/src/main/java/balendar/app/routes/AuthController.java
@@ -1,9 +1,5 @@
 package balendar.app.routes;
 
-import java.util.HashMap;
-
-import com.google.gson.Gson;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,11 +27,6 @@ public class AuthController {
 	public ResponseEntity<AuthTokenDTO> createAuthenticationToken(@RequestBody POSTAuthTokenDTO credentials) {
 		User user = userDetailsService.loadUserByUsername(credentials.username);
 		String authToken = JWTUtils.generateToken(user);
-
-		Gson gson = new Gson();
-
-		return ResponseEntity.ok(gson.fromJson(gson.toJson(new HashMap<String, String>() {{
-			put("authToken", authToken);
-		}}), AuthTokenDTO.class));
+		return ResponseEntity.ok(new AuthTokenDTO(authToken));
 	}
 }
