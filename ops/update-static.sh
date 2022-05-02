bb4a6efa6396b8afae8a5530ada9d5021eb66bb4
JoshuaXOng <joshuaong2000@gmail.com>
2022-05-02 21:39:29 +1000

    build: fixed script + new dist

--- ops/update-static.sh
+++ ops/update-static.sh
@@ -1,3 +1,5 @@
 cd ./website/
 yarn build
-mv ./dist/ ../server/app/src/main/resources/static/
+
+mkdir ../server/app/src/main/resources/static/ || (rm -r ../server/app/src/main/resources/static/ && mkdir ../server/app/src/main/resources/static/)
+mv ./dist/** ../server/app/src/main/resources/static/
