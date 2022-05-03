219ee69f431776dc1970afb5b070fa9a331bcb8e
JoshuaXOng <joshuaong2000@gmail.com>
2022-05-04 09:53:33 +1000

    feat: update snapshot db script

--- ops/snapshot-db-volume.sh
+++ ops/snapshot-db-volume.sh
@@ -1,2 +1,4 @@
-#give it a date and hash
-gsutil -o "Credentials:gs_service_key_file=$3" cp -r /var/lib/docker/volumes/balendar_database-data/ gs://$1$2
+printf -v date '%(%Y-%m-%d)T\n' -1
+echo $date
+
+gsutil -o "Credentials:gs_service_key_file=$1" cp -r /var/lib/docker/volumes/balendar_database-data/ gs://$2/databases/docker-volumes/$date
